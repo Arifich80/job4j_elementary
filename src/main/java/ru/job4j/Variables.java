877d54f15a007ec4001f7cd7f12e38adafbfb4a6
Arifich80 <arifich@yahoo.com>
2022-11-10 13:30:09 +0300

    4.2. Заголовок в файле java [#159036]

---
+++ src/main/java/ru/job4j/Variables.java
@@ -0,0 +1,13 @@
+package ru.job4j.calculate;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        int age = 18;
+        System.out.println(age);
+        float length = (float) 78.03332;
+        System.out.println(length);
+        boolean baby = true;
+        System.out.println(baby);
+    }
+}
