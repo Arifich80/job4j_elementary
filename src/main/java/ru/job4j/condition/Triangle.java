cb271af99c5c8e89ae8449a1495fe12865f347bc
Arifich80 <arifich@yahoo.com>
2022-11-15 17:54:39 +0300

    Множественное логическое выражение И [#9461]

---
+++ src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class Triangle {
+
+    public static boolean exist(double ab, double ac, double bc) {
+        return ((ab + ac > bc) && (ac + bc > ab) && (ab + bc > ac));
+    }
+
+    public static void main(String[] args) {
+
+        boolean result1 = LogicNot.isEven(3);
+        System.out.println(exist(1, 1, 1));
+        System.out.println(exist(2, 1, 1));
+        System.out.println(exist(1, 2, 1));
+        System.out.println(exist(1, 1, 2));
+    }
+}
