627df718f2b4bad617ecbfe715044630b5d66183
Arifich80 <arifich@yahoo.com>
2022-11-17 14:06:30 +0300

    Сумма чисел. [#226416]

---
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,21 @@
+package ru.job4j.loop;
+
+public class Counter {
+    public static int sum(int start, int finish) {
+        int sum = 0;
+        int i = start;
+        for (i = start; i <= finish; i++) {
+            sum = sum + start++;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(0, 10));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
+        System.out.println(sum(1, 2));
+        System.out.println(sum(2, 4));
+        System.out.println(sum(0, 6));
+    }
+}
