257a9957c0f67494f61ddaf364fe953de9fcb40b
Arifich80 <arifich@yahoo.com>
2022-11-22 19:20:12 +0300

    Упорядочить массив. [#4441]

---
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+public class FindLoop {
+    public static int indexOf(int[] data, int el) {
+        int rst = -1; /* если элемента нет в массиве, то возвращаем -1. */
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+
+    public static void main(String[] args) {
+        int[] data = new int[] {2, 4, 5};
+        FindLoop.indexOf(data, 2);
+
+    }
+}
