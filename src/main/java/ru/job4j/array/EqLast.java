8aef9e40de0664ca1d1d769aa52e3b826fac88b3
Arifich80 <arifich@yahoo.com>
2022-11-24 15:28:27 +0300

    Сравнить последние элементы двух массивов [#304228]

---
+++ src/main/java/ru/job4j/array/EqLast.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+public class EqLast {
+    public static boolean check(int[] left, int[] right) {
+        boolean result = true;
+        for (int i = 1; i < left.length; i++) {
+            if (left[left.length - 1] != right[right.length - 1]) {
+                result = false;
+
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] left = new int[]{2, 4, 1};
+        int[] right = new int[]{2, 4, 5};
+        System.out.println(EqLast.check(left, right));
+    }
+}
