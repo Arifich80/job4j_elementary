69eae60a83dfa543f0c88bc94801469113269983
Arifich80 <arifich@yahoo.com>
2022-12-01 19:34:57 +0300

    Дефрагментация массива [#225 #366105]

--- src/main/java/ru/job4j/array/Defragment.java
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -1,15 +1,13 @@
 package ru.job4j.array;
-
 public class Defragment {
     public static String[] compress(String[] array) {
         for (int index = 0; index < array.length; index++) {
             if (array[index] == null) {
-                /* переместить первую не null ячейку. Нужен цикл. */
-                for (int i = index; i < array.length; i++) {
+                for (int i = index + 1; i < array.length; i++) {
                     if (array[i] != null) {
                         array[index] = array[i];
                         array[i] = null;
-                        return array;
+                        break;
                     }
                 }
             }
