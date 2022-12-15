dd21bbef21024f664679e603937e482cdfbb818f
Arifich80 <arifich@yahoo.com>
2022-12-15 21:42:26 +0300

    Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -7,8 +7,19 @@ public class Max {
 
     }
 
+    public static int max(int left, int right, int third) {
+        int result = Math.max(left, right) > third ? Math.max(left, right) : third;
+        return result;
+    }
+
+    public static int max(int left, int right, int third, int fourth) {
+        int result = Math.max(Math.max(left, right), third) > fourth ? Math.max(Math.max(left, right), third) : fourth;
+        return result;
+    }
+
     public static void main(String[] args) {
-        int rsl = Max.max(1, 2);
+        int rsl = Max.max(1, 2, 3, 5);
         System.out.println(rsl);
+
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -15,11 +15,11 @@ public class MaxTest {
 
     @Test
     public void whenMax5To3Then5() {
-            int left = 5;
-            int right = 3;
-            int result = Max.max(left, right);
-            int expected = 5;
-            assertThat(result).isEqualTo(expected);
+        int left = 5;
+        int right = 3;
+        int result = Max.max(left, right);
+        int expected = 5;
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
@@ -30,5 +30,26 @@ public class MaxTest {
         int expected = 6;
         assertThat(result).isEqualTo(expected);
 
+    }
+
+    @Test
+    public void whenMax3To1To2Then3() {
+        int left = 1;
+        int right = 2;
+        int third = 3;
+        int result = Math.max(left, right) > third ? Math.max(left, right) : third;
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax5To1To2To3Then5() {
+        int left = 1;
+        int right = 2;
+        int third = 3;
+        int fourth = 5;
+        int result = Math.max(Math.max(left, right), third) > fourth ? Math.max(Math.max(left, right), third) : fourth;
+        int expected = 5;
+        assertThat(result).isEqualTo(expected);
     }
 }
